6cf9600b4411f4eb0a139a7e13a0d9013926c6ac
h5c52 <h5c52@protonmail.com>
2023-03-29 00:10:21 -0400

    wallpaper

--- riceScript.sh
+++ riceScript.sh
@@ -12,17 +12,21 @@ cp files/.bashrc /home/$user/.bashrc
 cp files/wallpaper.png $user.config/wallpaper.png
 
 #Install Xorg packages
-pacman --noconfirm --needed -S base-devel git xorg-server xorg-xinit libx11 libxinerama libxft webkit2gtk
+pacman --noconfirm --needed -S base-devel git xorg-server xorg-xinit libx11 libxinerama libxft webkit2gtk xwallpaper
 echo "exec dwm" >> /home/$user/.xinitrc
 
 #Install and configure dwm Window Manager
 mkdir /home/$user/bin
 cd /home/$user/bin
-
-git clone https://git.suckless.org/dwm
+	
+git clone https://github.com/h5c52/dwm
 git clone https://git.suckless.org/dmenu
+git clone https://github.com/h5c52/st
 
 cd dwm
+make clean install
+
+cd ../dmenu
 make clean install
 
 cd ../dmenu
